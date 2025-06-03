70be503afc8997f7d0fa43350c113ccaa7b3e840
SaiSanjay77 <sunsanjay2007@gmail.com>
2025-06-03 18:18:46 +0530

    Trial Java program in Intellij IDEA

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Yo, WebStorm Java is alive!");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
