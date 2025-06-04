c1eef70cadd335466e0bd8649125dd69062cd527
SaiSanjay77 <sunsanjay2007@gmail.com>
2025-06-04 12:34:02 +0530

    just thought

---
+++ teaching.js
@@ -0,0 +1,32 @@
+/*let a = "sai"
+let b = "coder"
+let c = a + ' ' + b
+console.log(c)*/
+/*let a="nandita"
+let b="coder"
+let c=a+" "+b
+console.log(c)*/
+// Template literals
+//let a ="nandita"
+//console.log(`hello ${a}`)
+/*let a = "paul"
+let b = "sai"
+let c ="nithish"
+console.log(`hello ${a}, hello ${b}, hello ${c}`)*/
+//let a = 69
+//let b = "sai"
+//console.log(typeof a)
+//console.log((2**50)>(3**35))
+//console.log(((2**30)!=(3**30)) && ((2**7)!=(7**3)))
+//Ternary operator
+//let a = 10
+//let b = 20
+//let c = a>b ? a : b
+//console.log(c)
+//let a = 10
+//let b = 20
+//let c = a>b ? a : b
+//console.log(c)
+let a = 2**5
+let b = 3**3
+console.log(a>b?a:b)
