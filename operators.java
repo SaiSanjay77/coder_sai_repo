931aadc87ad225746cdf4829502cdd458cef935d
SaiSanjay77 <sunsanjay2007@gmail.com>
2025-06-05 17:56:39 +0530

    Added some learning files

--- operators.java
+++ operators.java
@@ -1,4 +1,4 @@
-public class test {
+public class operators{
     public static void main(String[] args) {
         int a=10 ;
         int b=20 ;
@@ -10,5 +10,11 @@ public class test {
         System.out.println(a-b);
         System.out.println("The quotient of a and b is:");
         System.out.println(a%b);
+        // getting fractional part
+        int c = 30;
+        int d = 9;
+        System.out.println("The fractional part of c/d is:");
+        double e = (double)c/d;
+        System.out.println(e);
     }
 }
