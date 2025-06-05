09d3f90fc89b6ac322d09d7becc1243996208cd6
SaiSanjay77 <sunsanjay2007@gmail.com>
2025-06-05 15:30:23 +0530

    Added some learning files

---
+++ area_of_circle.java
@@ -0,0 +1,8 @@
+public class area_of_circle {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double r = 69.0;
+        double area = pi * r * r;
+        System.out.println("The area of the circle is: " + area);
+    }
+}
