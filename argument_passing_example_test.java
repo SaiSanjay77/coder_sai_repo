920f9534a7f27db43577f335aa4f79f5acf72ef7
SaiSanjay77 <sunsanjay2007@gmail.com>
2025-06-07 09:49:52 +0530

    Added some learning files

---
+++ argument_passing_example_test.java
@@ -0,0 +1,20 @@
+class argument_passing_example {
+    void test(int a, int b)
+    {
+        a = a/2;
+        b = b*2;
+    }
+}
+public class argument_passing_example_test {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a is: " + a);
+        System.out.println("b is: " + b);
+        argument_passing_example obj = new argument_passing_example();
+        obj.test(a, b);
+        //There is no change in this thing since a and b are primitive data types.
+        System.out.println("a is: " + a);
+        System.out.println("b is: " + b);s
+    }
+}
